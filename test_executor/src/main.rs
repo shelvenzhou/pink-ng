2926120ab40f14f2ecf6b784dda54a6ce1ae7d7f
Kowalski Dark <darkkowalski2012@gmail.com>
2021-05-19 20:20:56 +0800

    It does compile

--- test_executor/src/main.rs
+++ test_executor/src/main.rs
@@ -2,18 +2,24 @@ use sc_executor;
 
 use sc_executor::{WasmExecutor, WasmExecutionMethod};
 use sp_wasm_interface::{HostFunctions, Function};
+use sp_runtime_interface::runtime_interface;
 
-pub type SubstrateHostFunctions = (
-	sandbox::HostFunctions,
-);
+
+#[runtime_interface(no_tracing)]
+trait MyInterface {
+    fn say_hello_world(data: &str) {
+        println!("Hello world from: {}", data);
+    }
+}
 
 fn main() {
+
     let wasm_method = WasmExecutionMethod::Interpreted;
     let default_heap_pages = Some(17);
     let executor = WasmExecutor::new(
         wasm_method,
         default_heap_pages,
-        SubstrateHostFunctions::host_functions(),
+        my_interface::HostFunctions::host_functions(),
         8,
         None,
     );
